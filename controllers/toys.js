686c4781df9436b0d48cde07c9653b8cc69622e2
Padmasri <padmasri2101@gmail.com>
2024-04-05 12:35:45 -0500

    Update toys.js

--- controllers/toys.js
+++ controllers/toys.js
@@ -4,9 +4,18 @@ exports.toys_list = function(req, res) {
  res.send('NOT IMPLEMENTED: toys list');
 };
 // for a specific toys.
-exports.toys_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: toys detail: ' + req.params.id);
-};
+exports.toys_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await toys.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+
 // Handle toys create on POST.
 exports.toys_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: toys create POST');
@@ -17,11 +26,27 @@ exports.toys_delete = function(req, res) {
 };
  
 // Handle toys update form on PUT.
-exports.toys_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: toys update PUT' + req.params.id);
-   };
- 
- 
+exports.toys_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await toys.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.toys_type)
+    toUpdate.toys_type = req.body.toys_type;
+    if(req.body.size) toUpdate.size = req.body.size;
+    if(req.body.price) toUpdate.price = req.body.price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+    
+
 exports.toys_list = async function(req, res) {
     try{
     thetoys = await toys.find();
